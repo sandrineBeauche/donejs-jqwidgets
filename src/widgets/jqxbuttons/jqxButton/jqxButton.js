8c2d009bdee60024287bd413a3ab42f466fd4277
Sandrine Beauche <sandrine.beauche@gmail.com>
2017-07-18 17:47:26 +0200

    add binding to properties

--- src/widgets/jqxbuttons/jqxButton/jqxButton.js
+++ src/widgets/jqxbuttons/jqxButton/jqxButton.js
@@ -1,6 +1,3 @@
-
-
-
 import 'jqwidgets-framework/jqwidgets/styles/jqx.base.css!';
 
 import $ from 'jquery';
@@ -8,31 +5,39 @@ import '../jqxbuttons.js';
 
 import Component from 'can-component';
 import DefineMap from 'can-define/map/';
-
-
+import DefineList from 'can-define/list/';
+import Control from 'can-control';
+import each from 'can-util/js/each/each';
 
 import './jqxButton.less';
 import view from './jqxButton.stache';
 
-
-
-
-
 export const ViewModel = DefineMap.extend({
 
+});
+
+export const ControlComp = Control.extend({
+  init: function(element, options){
+    this.prepareBindings();
+  },
+  prepareBindings: function(){
+    let args = this.viewModel.serialize();
+    each(args, function(value, key) {
+      this.viewModel.on(key, this.setNewVal);
+    });
+  },
+  setNewVal: function(ev, attr, how, newVal){
+    let newArgs = {};
+    newArgs[attr] = newVal;
+    $(this.element.firstElementChild).jqxButton(newArgs);
+  }
 });
 
 export default Component.extend({
   tag: 'jqxButton',
   ViewModel,
   view,
-  events: {
-    init: function(element, options){
-      //let args = this.viewModel.serialize();
-
-
-    }
-  },
+  ControlComp,
   init: function(element){
     let args = this.viewModel.serialize();
     $(element.firstElementChild).jqxButton(args);
